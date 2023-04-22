e9e31844db5e82903fced835c70cfc221aaeb54b
AM111D <Dimasssnike@icloud.com>
2023-04-22 12:17:10 -0700

    Update Contacts.js

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -31,5 +31,5 @@ Contacts.propTypes = {
     }),
   ).isRequired,
   filter: PropTypes.string.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
+  ondeletContact: PropTypes.func.isRequired,
 };
